0e214d5cde6f05b4f62a3e488876acaa7e38786b
Natan Brito <natanismaelbrito@gmail.com>
2022-07-23 13:48:11 -0300

    feat:getDisciplinesTests make a query db for get correct order

--- src/repositorys/TestsRepository.ts
+++ src/repositorys/TestsRepository.ts
@@ -1,7 +1,11 @@
 import { tests } from "@prisma/client";
 import prisma from "../config/db.js";
 
-export type dataTest = Omit<tests, "id">;
-export async function createTest(data: dataTest) {
+export type dataCreateTest = Omit<tests, "id">;
+export async function createTest(data: dataCreateTest) {
   return await prisma.tests.create({ data });
+}
+export async function getByDisciplines() {
+  // return await prisma.tests.();
+  //   TODO fazer a query pra buscar no banco ordenado por disciplinas
 }
--- src/services/TestsService.ts
+++ src/services/TestsService.ts
@@ -1,5 +1,8 @@
 import * as testsRepository from "../repositorys/TestsRepository.js";
 
-export async function createTest(data: testsRepository.dataTest) {
+export async function createTest(data: testsRepository.dataCreateTest) {
   return await testsRepository.createTest(data);
+}
+export async function OrderByDiscisplinesTests() {
+  return await testsRepository.getByDisciplines();
 }
