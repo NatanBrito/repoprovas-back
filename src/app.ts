9df2185ab947366a7732d64ae12895d8b5e93f49
Natan Brito <natanismaelbrito@gmail.com>
2022-07-24 16:45:06 -0300

    add cors

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,11 @@ import "express-async-errors";
 
 import dotenv from "dotenv";
 import routes from "./Routes/index.js";
-// import { cors } from "cors";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
-// app.use(cors());
+app.use(cors());
 app.use(json());
 app.use(routes);
 
